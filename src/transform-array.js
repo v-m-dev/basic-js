8cb76c21e3440694c99d0b9b59bb37a9f9433f29
v-m-dev <morozvitalii2403@gmail.com>
2023-12-23 21:22:31 +0200

    feat: complete transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,65 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let transformedArr = [...arr];
+
+  for (let i = 0; i < transformedArr.length; i++) {
+    if (transformedArr[i] === "--discard-prev") {
+      if (i === 0) {
+        transformedArr.splice(i, 1);
+        i = 0;
+      } else {
+        transformedArr[i - 1] = "";
+        transformedArr.splice(i, 1);
+        i = 0;
+      }
+    } else if (transformedArr[i] === "--discard-next") {
+      if (i === transformedArr.length - 1) {
+        transformedArr.splice(i, 1);
+        i = 0;
+      } else {
+        transformedArr[i + 1] = "";
+        transformedArr.splice(i, 1);
+        i = 0;
+      }
+    } else if (transformedArr[i] === "--double-prev") {
+      if (i === 0) {
+        transformedArr.splice(i, 1);
+        i = 0;
+      } else {
+        transformedArr[i] = transformedArr[i - 1];
+        i = 0;
+      }
+    } else if (transformedArr[i] === "--double-next") {
+      if (i === transformedArr.length - 1) {
+        transformedArr.splice(i, 1);
+        i = 0;
+      } else {
+        transformedArr[i] = transformedArr[i + 1];
+        i = 0;
+      }
+    }
+  }
+  return transformedArr.filter((item) => item !== "");
 }
 
 module.exports = {
-  transform
+  transform,
 };
